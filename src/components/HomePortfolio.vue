244336dacd3fdb5dd3196964ec3cc2ee113a388f
arabDevCom <semicolonsales@gmail.com>
2022-04-14 12:23:36 +0200

    home

--- src/components/HomePortfolio.vue
+++ src/components/HomePortfolio.vue
@@ -6,13 +6,28 @@
       </div>
       <p class="coTitle mb-5">نستعرض لك بعض الاعمال التي قمنا بتنفيذها ...</p>
       <div class="row">
-        <div class="col-md-3 p-2" v-for="Project in Projects" :key="Project">
+        <div class="col-6 col-sm-4 col-md-3 p-1 p-md-2" v-for="Project in Projects.slice(0, 7)" :key="Project" >
           <div class="project">
-            <img :src="Project.image" />
-            <div class="projectData">
-              <h4>{{ Project.title }}</h4>
+            <div class="projectDetails">
+              <img :src="Project.image" />
+              <div class="details">
+                <h4>{{ Project.title }}</h4>
+                <span v-for="item in Project.type" :key="item">
+                  {{ item }}
+                </span>
+              </div>
+              <div class="details details-2">
+                <p>{{ Project.description }}</p>
+              </div>
             </div>
           </div>
+        </div>
+        <div class="col-6 col-sm-4 col-md-3 p-1 p-md-2">
+          <router-link to="/portfolio">
+            <div class="showAll">
+              <h6>عرض الكل <i class="fa-duotone fa-left-long ms-2"></i></h6>
+            </div>
+          </router-link>
         </div>
       </div>
     </div>
@@ -38,108 +53,110 @@ export default {
 <style lang="scss" scoped>
 .HomePortfolio {
   padding: 70px 0;
+
   .project {
-    padding: 20px;
-    border-radius: 5px;
-    box-shadow: 0px 0px 10px #0000001a;
-    background-color: #fff;
-    position: relative;
-    &::after {
-      height: 100%;
-      content: "";
-      left: 0px;
-      position: absolute;
-      top: 0px;
-      width: 100%;
-      transition: all 0.3s ease-in-out;
-      opacity: 0;
-      border-radius: 5px;
-      background-size: cover;
-      background-position: center;
-      z-index: 1;
-      background-image: linear-gradient(60deg, #ffaef2d5, #ff9394d5);
-    }
+    border-radius: 10px;
+    transition: all 0.3s ease-in-out;
+
     img {
-      position: relative;
+      border-radius: 10px;
+      height: 300px;
+      width: 100%;
+      object-fit: cover;
+    }
+    .details {
+      bottom: 25px;
+      right: 10px;
       z-index: 2;
-      width: 100%;
-      height: 250px;
-    }
-    .projectData {
+      transition: all 0.3s ease-in-out;
       position: absolute;
-      left: 0;
-      top: 0;
-      width: 100%;
-      background-color: #fff;
-      z-index: 3;
-      padding: 10px;
-      text-align: center;
-      box-shadow: 0px 4px 12px #0000001a;
+
+      h4 {
+        color: #ffffff;
+        font-size: 20px;
+        margin-bottom: 0px;
+      }
+      span {
+        color: #dadde3;
+        margin: 3px;
+        display: inline-block;
+        text-transform: uppercase;
+      }
+      p {
+        color: #ffffff;
+        padding: 0 10px;
+        max-height: 250px;
+      }
     }
-    &:hover{
+    .details-2 {
+      opacity: 0;
+      top: 0px;
+      visibility: hidden;
+    }
+    .projectDetails {
+      position: relative;
+      &::after,
+      &::before {
+        content: "";
+        height: 100%;
+        right: 0px;
+        position: absolute;
+        width: 100%;
+        transition: all 0.3s ease-in-out;
+        border-radius: 8px;
+      }
+      &::before {
+        background-image: linear-gradient(60deg, #f074dcd5, #ed272ad5);
+        opacity: 0;
+        right: 0px;
+      }
       &::after {
-        opacity: 1;
+        background-image: linear-gradient(0deg, #1a152e, #1a152e00);
+        top: 0px;
+        z-index: 1;
       }
     }
 
-    // .content {
-    //   position: relative;
-    //   z-index: 2;
-    //   transition: all 0.3s ease-in-out;
+    &:hover {
+      .projectDetails::before {
+        opacity: 0.9;
+      }
+      .projectDetails::after {
+        opacity: 0;
+      }
+      .details {
+        bottom: 0px;
+        opacity: 0;
+        visibility: hidden;
+      }
+      .details-2 {
+        opacity: 1;
+        top: 25px;
+        visibility: visible;
+      }
+    }
+  }
+  .showAll {
+    width: 100%;
+    height: 100%;
+    border-radius: 200px;
+    text-align: center;
+    padding: 10px 0;
+    transition: all 0.3s ease-in-out;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #1a152e00;
+    color: #000000;
+    border: 1px solid #000000;
 
-    //   .icon {
-    //     width: 40px;
-    //     height: 40px;
-    //     display: flex;
-    //     justify-content: center;
-    //     align-items: center;
-    //     margin: 30px 10px;
-    //     transition: all 0.3s ease-in-out;
-
-    //     i {
-    //       font-size: 30px;
-    //       color: #ed2729;
-    //     }
-    //   }
-    //   h3 {
-    //     font-size: 30px;
-    //     font-weight: bold;
-    //     margin-bottom: 10px;
-    //     transition: all 0.3s ease-in-out;
-    //   }
-    //   p {
-    //     font-size: 16px;
-    //     line-height: 30px;
-    //     transition: all 0.3s ease-in-out;
-    //   }
-    //   a {
-    //     padding: 20px 0px 0;
-    //     display: block;
-    //     transition: all 0.3s ease-in-out;
-
-    //     i {
-    //       font-size: 20px;
-    //       color: #ed2729;
-    //     }
-    //   }
-    // }
-    // &:hover {
-    //   &::after {
-    //     opacity: 1;
-    //   }
-    //   .content {
-    //     a,
-    //     .icon {
-    //       i {
-    //         color: #fff;
-    //       }
-    //     }
-    //     p,
-    //     h3 {
-    //       color: #fff;
-    //     }
-    //   }
-    // }
+    &:hover {
+      color: #ffffff;
+      background-color: #1a152e;
+      box-shadow: 0px 10px 30px #00000030;
+      border-radius: 10px;
+    }
   }
 }
 </style>
